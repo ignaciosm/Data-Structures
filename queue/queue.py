f13d2d39d08bb4b64f787bbd7575c51449e0bf6f
Ignacio San Martin <ignaciosm@gmail.com>
2020-06-11 15:52:12 -0500

    refactor contains

--- queue/queue.py
+++ queue/queue.py
@@ -15,7 +15,9 @@ Stretch: What if you could only use instances of your Stack class to implement t
 """
 
 
-
+# AS A LINKED LIST
+import sys
+sys.path.append('../singly_linked_list')
 from singly_linked_list import LinkedList
 
 class Queue:
@@ -37,8 +39,6 @@ class Queue:
         else:
             pass
 
-
-
 # q = Queue()
 # print(q.size)
 # q.enqueue(1)
@@ -52,26 +52,26 @@ class Queue:
 # print(q.storage.tail.value)
 
 
-# class Queue:
-#     def __init__(self):
-#         self.size = 0
-#         self.storage = []
+# AS AN ARRAY
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
     
-#     def __len__(self):
-#         return self.size
+    def __len__(self):
+        return self.size
 
-#     def enqueue(self, value):
-#         self.size += 1
-#         self.storage.append(value)
+    def enqueue(self, value):
+        self.size += 1
+        self.storage.append(value)
 
-#     def dequeue(self):
-#         if self.storage:
-#             self.size -= 1
-#             return self.storage.pop(0)
-#         else:
-#             pass
-
-
+    def dequeue(self):
+        if self.storage:
+            self.size -= 1
+            return self.storage.pop(0)
+        else:
+            pass
 
 # q = Queue()
 # q.enqueue(1)
